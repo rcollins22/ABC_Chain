be107c456771e1197993145d0ceaa5ba969c8ba8
Rashad Collins <rashad.collins22@gmail.com>
2020-06-10 12:37:00 -0400

    updated secuirty measures

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 import hashlib
 import datetime
+from wallets import
 
 class Block:
     block_id = 0
+    prev_block = None
+    next_block=None
     confirm_tran=None
+    info=None
+    idx=0
+    time=datetime.datetime.now
+
+    def __init__(self,info)
+        self.info=info
+       
+
+    def sec(self):
+         h=hashlib.sha256()
+         h.verify(
+             str(self.block_id) +
+
+         )
+
+
+
+
     
 
 
