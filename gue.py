783b48972c8532a15e4539f8d6ca4d035ece261c
Rashad Collins <rashad.collins22@gmail.com>
2020-06-11 00:04:15 -0400

    non tokenized operation

--- gue.py
+++ gue.py
@@ -3,53 +3,48 @@ import hashlib
 
 class Block:
     block_id = 0
-    trx = 'ddmdkvf4545m45m4kd4krm5km567476nb8'
+    trx = '0'
     next = None
     hash = None
     idx = 0
     previous_hash = 0x0
     timestamp = datetime.datetime.now()
 
-    ##INITIALIZES THE INFO FROM THE TRANSACTION##
     def __init__(self, trx):
         self.trx = trx
 
     def hash(self):
         h = hashlib.sha256()
-        ##ADD NUMEROUS VARIABLES BELOW FROM PREVIOUS BLOCK TO DEEPEN CHAIN ECRYPTION##
         h.update(
         str(self.idx).encode() +
         str(self.trx).encode() +
         str(self.previous_hash).encode() +
         str(self.timestamp).encode() +
-        str(self.block_id).encode())
-        ##RETURNS THE ENCRYPTED HASH CODE##
+        str(self.block_id).encode()
+        )
         return h.hexdigest()
 
     def __str__(self):
         return "Block Hash: " + str(self.hash()) + "\nBlockNo: " + str(self.block_id) + "\nBlock Data: " + str(self.trx) + "\nHashes: " + str(self.idx) + "\n--------------"
 
 class ABCchain:
-        ##THIS FUNCTION HANDLES THE THE VALIDATION OF A TRANSACTION##
-    diff = 20      ##ALLOWS US TO ADJUST THE DIFFICULTY OF SOLVING THE HASH##
+
+    diff = 20
     max_idx = 2**32
     target = 2 ** (256-diff)
-    ##CREATES THE STARTING BLOCK OR 'GENESIS' BLOCK##
+
     block = Block("First")
-    ##ALLOWS THE VARIABLES TO NOT CONSTANTLY BE SET TO EACH OTHER, FOR A 'not' LOOP LATER##
     hide = head = block
 
-    ##THIS FUNCTION CONNECTS THE CURRENT BLOCK TO THE NEXT ONE, SOLIDIFYING CHAIN ENCRYPTION##
     def add(self, block):
 
-        block.previous_hash = self.block.hash() ##PREV. BLOCK SET TO CURRENT BLOCK##
-        block.block_id = self.block.block_id + 1  ##CHANGE THE ID TO THE NEXT BLOCK##
+        block.previous_hash = self.block.hash()
+        block.block_id = self.block.block_id + 1
 
-        self.block.next = block     ##VARIABLE CREATED FOR THE NEXT BLOCK##
-        self.block = self.block.next     ##CONTINUALLY SETS THE NEXT BLOCK HASH##
+        self.block.next = block
+        self.block = self.block.next
 
-    ##THIS FUNCTION WILL ACT AS THE MINING FEATURE##
-    def mining(self, block):
+    def mine(self, block):
         for n in range(self.max_idx):
             if int(block.hash(), 16) <= self.target:
                 self.add(block)
@@ -61,7 +56,7 @@ class ABCchain:
 ABCchain = ABCchain()
 
 for n in range(1):
-    ABCchain.mining(Block("Block " + str(n+1)))
+    ABCchain.mine(Block("Block " + str(n+1)))
 
 while ABCchain.head != None:
     print(ABCchain.head)
--- wallets.py
+++ wallets.py
@@ -4,8 +4,9 @@ import datetime
 
 class Wallets():
     ##PULLS 'name' STRING FROM 'wallet#' VARIABLE TO CREATE UNIQUE WALLET TRANSACTION##
-    def __init__(self,name,balance=10):
+    def __init__(self,name,balance):
         self.name=name
+        self.balance=balance
     ##FOW NOW TRANSACTION IS NESTED IN CLASS. WILL HAVE OWN FUNCTION##    
     def encrypt_trans(self):
         signature = hashlib.sha256(self.name.encode()).hexdigest()
@@ -16,11 +17,13 @@ class Wallets():
 
 
 
-wallet1 = Wallets('Clint')
-wallet2=Wallets('Rashad')
-wallet3=Wallets('Sean')
-wallet4=Wallets('Satoshi')
-wallet5=Wallets('Adnan')
-wallet6=Wallets('Bob')
+wallet1 = Wallets('Clint',10)
+wallet2=Wallets('Rashad',10)
+wallet3=Wallets('Sean',10)
+mywall=Wallets('User',10)
 import_code=wallet1.encrypt_trans()
 print(import_code)
+
+#def transaction():
+
+
